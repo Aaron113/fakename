8b823eca8c2f831b6e1b58aa763f9b578ace3c3f
Aaron <xaaron113x@gmail.com>
2015-04-14 08:15:36 -0500

    Code Cleanup  + Added meta:RealNick() - returns real name  + Added meta:ClearFakename() - removes all fake values  + Added meta:IsFakenamed()

--- lua/ulx/modules/sh/sh_fakename.lua
+++ lua/ulx/modules/sh/sh_fakename.lua
@@ -4,16 +4,25 @@
 
 local meta = FindMetaTable("Player") 
 
-function meta:SetFakename(name) 
-	self:SetNWString("ufakename_name", name) 
-end 
-
 oldGetUserGroup = meta.GetUserGroup 
 function meta:GetUserGroup() 
 	return self:GetNWString("ufakename_group", false) or oldGetUserGroup(self) 
 end 
 newGetUserGroup = meta.GetUserGroup 
 meta.GetUserGroup = oldGetUserGroup
+
+local oldnick = meta.Nick 
+function meta:Nick() 
+	return self:GetNWString("ufakename_name", false) or oldnick(self) 
+end 
+
+function meta:RealNick() 
+	return oldnick(self) 
+end 
+
+function meta:SetFakename(name) 
+	self:SetNWString("ufakename_name", name) 
+end 
 
 function meta:SetFakegroup(group) 
 	meta.GetUserGroup = newGetUserGroup 
@@ -25,16 +34,23 @@ function meta:SetFakegroup(group)
 end 
 
 function meta:RemoveFakename() 
-	self:SetNWString("ufakename_name", nil) 
-	self:SetNWString("ufakename_group", nil) 
+	self:SetNWString("ufakename_name", false) 
+end 
+
+function meta:RemoveFakegroup() 
+	self:SetNWString("ufakename_group", false) 
 	if ulx.teams then 
 		hook.GetTable().PlayerSpawn.UTeamSpawnAuth(self) 
 	end 
 end 
 
-local oldnick = meta.Nick 
-function meta:Nick() 
-	return self:GetNWString("ufakename_name", false) or oldnick(self) 
+function meta:ClearFakename() 
+	self:RemoveFakename() 
+	self:RemoveFakegroup() 
+end 
+
+function meta:IsFakenamed() 
+	return self:GetNWString("ufakename_name", false) or self:GetNWString("ufakename_group", false) 
 end 
 
 function ulx.fakename(calling_ply, name, group) 
@@ -49,8 +65,10 @@ fakename:defaultAccess( ULib.ACCESS_SUPERADMIN )
 fakename:help( "Add a user to specified group." )
 
 function ulx.unfakename(calling_ply) 
-	calling_ply:RemoveFakename() 
-	ulx.fancyLogAdmin(calling_ply, true, "#A unfakenamed") 
+	if calling_ply:IsFakenamed() then 
+		calling_ply:RemoveFakename() 
+		ulx.fancyLogAdmin(calling_ply, true, "#A unfakenamed") 
+	end 
 end 
 local unfakename = ulx.command( "Utility", "ulx unfakename", ulx.unfakename )
 unfakename:defaultAccess( ULib.ACCESS_SUPERADMIN )
