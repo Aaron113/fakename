d60d19b9f3d9f77cffa8654ca8405a6cdbad28f8
Aaron <xaaron113x@gmail.com>
2015-04-14 23:21:04 -0500

    Added useful functions...  + Added Player:GetFakename()  + Added Player:GetFakegroup()  * Changed Player:RemoveFake[name/group]() to use Player:SetFakename(false)  * Changed command descriptions from default  * Changed default fake group to own group

--- lua/ulx/modules/sh/sh_fakename.lua
+++ lua/ulx/modules/sh/sh_fakename.lua
@@ -33,15 +33,20 @@ function meta:SetFakegroup(group)
 	meta.GetUserGroup = oldGetUserGroup 
 end 
 
+function meta:GetFakename() 
+	return self:GetNWString("fakename_name", self:Nick()) 
+end 
+
+function meta:GetFakegroup() 
+	return self:GetNWString("fakename_group", self:GetUserGroup()) 
+end 
+
 function meta:RemoveFakename() 
-	self:SetNWString("fakename_name", false) 
+	self:SetFakename(false) 
 end 
 
 function meta:RemoveFakegroup() 
-	self:SetNWString("fakename_group", false) 
-	if ulx.teams then 
-		hook.GetTable().PlayerSpawn.UTeamSpawnAuth(self) 
-	end 
+	self:SetFakegroup(false) 
 end 
 
 function meta:ClearFakename() 
@@ -50,26 +55,30 @@ function meta:ClearFakename()
 end 
 
 function meta:IsFakenamed() 
-	return self:GetNWString("fakename_name", false) or self:GetNWString("fakename_group", false) 
+	return (self:GetNWString("fakename_name", false) or self:GetNWString("fakename_group", false))
 end 
 
 function ulx.fakename(calling_ply, name, group) 
+	if !group or group == "" then 
+		group = calling_ply:GetUserGroup() 
+	end 
+
 	ulx.fancyLogAdmin(calling_ply, true, "#A fakenamed themselves #s in group #s", name, group) 
 	calling_ply:SetFakename(name) 
 	calling_ply:SetFakegroup(group) 
 end 
 local fakename = ulx.command( "Utility", "ulx fakename", ulx.fakename )
 fakename:addParam{ type=ULib.cmds.StringArg, hint="fake name" }
-fakename:addParam{ type=ULib.cmds.StringArg, completes=ulx.group_names, hint="group", error="invalid group \"%s\" specified" }
+fakename:addParam{ type=ULib.cmds.StringArg, completes=ulx.group_names, hint="group", error="invalid group \"%s\" specified", ULib.cmds.optional }
 fakename:defaultAccess( ULib.ACCESS_SUPERADMIN )
-fakename:help( "Add a user to specified group." )
+fakename:help( "Changes your in-game name and group." )
 
 function ulx.unfakename(calling_ply) 
 	if calling_ply:IsFakenamed() then 
-		calling_ply:RemoveFakename() 
+		calling_ply:ClearFakename() 
 		ulx.fancyLogAdmin(calling_ply, true, "#A unfakenamed") 
 	end 
 end 
 local unfakename = ulx.command( "Utility", "ulx unfakename", ulx.unfakename )
 unfakename:defaultAccess( ULib.ACCESS_SUPERADMIN )
-unfakename:help( "Add a user to specified group." )
+unfakename:help( "Removes any fake names or groups" )
